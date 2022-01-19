8badeb1bcb3f704395a2dae9870af1f13174763e
sneharatnani <sneharatnani5@gmail.com>
2022-01-19 18:18:52 +0530

    added keyboard events

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
+let point = document.querySelector('#point');
+let backSpace = document.querySelector('#backspace');
 let operators = document.querySelectorAll('.operator');
 let display = document.querySelector('.display');
 let numbers = document.querySelectorAll('.num');
@@ -5,59 +7,57 @@ let clear = document.querySelector('.clear');
 let equals = document.querySelector('#equals');
 let total = 0;
 let numOne = [];
-// let numTwo = '';
 let operator = [];
-let container = '';
+let container = [];
 
 clear.addEventListener('click', restart);
 function restart() {
-    numOne = '';
+    numOne = [];
+    container = [];
     display.textContent = '';
     total = 0;
+    operator = [];
 }
 
+function getNumbers(e) {
+    if (container.length < 20) {
+        container.push(e.target.textContent);
+        display.textContent = container.join('');
+        numOne.push(e.target.textContent);
+    }
+}
 numbers.forEach((num) => {
-    num.addEventListener('click', () => {
-        container += num.textContent;
-        display.textContent = container;
-        numOne.push(num.textContent);
-    });
+    num.addEventListener('click', getNumbers);
 });
 
+function givePoint() {
+    if (!container.includes('.') && container.length < 20) {
+        container.push('.');
+        display.textContent = container.join('');
+        numOne.push('.');
+    }
+}
+point.addEventListener('click', givePoint);
 
+backSpace.addEventListener('click', eraseLast);
+function eraseLast() {
+    console.log(numOne, container);
+    numOne.pop();
+    container.pop();
+    display.textContent = container.join('');
+    console.log(numOne, container);
+}
 
 operators.forEach((op) => {
     op.addEventListener('click', () => {
-        /*  if (e.target.textContent !== '=') {
-             operator = e.target.textContent;
-         }
-         else {
-             switch (operator) {
-                 case '+':
-                     operate();
-                     total = '';
-                     console.log(numOne, total);
-                     break;
-             }
-         } */
         operator.push(op.textContent);
         numOne.push('new');
-        container = '';
+        container = [];
     });
 });
 
 
 function operate() {
-
-}
-
-function clearDisplay() {
-    display.textContent = '';
-    // display.textContent = total;
-
-}
-
-equals.addEventListener('click', () => {
     let finalNums = numOne.join('').split('new');
     total = Number(finalNums.shift());
     for (let i = 0; i < finalNums.length; i++) {
@@ -92,20 +92,69 @@ equals.addEventListener('click', () => {
                 }
                 break;
         }
-
     }
-
+    showTotal();
     console.log(total, operator, numOne);
-});
-/* function show() {
-    if (total === 'Infinity') {
+}
+
+function showTotal() {
+    if (total === Infinity) {
         display.textContent = 'ERROR';
     }
-    else {
+    else if (Number(total) === total && total % 1 !== 0) {
         display.textContent = total.toFixed(2);
     }
+    else {
+        display.textContent = total;
+    }
 }
- */
+
+equals.addEventListener('click', operate);
+
+
+
+// keyboard support
+document.addEventListener('keydown', (e) => {
+    let numArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let signArr = ['+', '-',];
+    switch (true) {
+        case numArr.includes(e.key) && container.length < 20:
+            container.push(e.key);
+            numOne.push(e.key);
+            display.textContent = container.join('');
+            break;
+
+        case signArr.includes(e.key):
+            operator.push(e.key);
+            container = [];
+            numOne.push('new');
+            break;
+
+        case e.key === '/':
+            operator.push('÷');
+            container = [];
+            numOne.push('new');
+            break;
+
+        case e.key === '*':
+            operator.push('×');
+            container = [];
+            numOne.push('new');
+            break;
+
+        case e.key === '=':
+            operate();
+            break;
+
+        case e.key === 'Backspace':
+            eraseLast()
+            break;
+
+        case e.key === '.':
+            givePoint();
+    }
+});
+
 
 
 
